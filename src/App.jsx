3f89545916ddaebd8decb1c1c38d245b39f6b7ce
valera56 <tenaevv1@gmail.com>
2024-02-26 16:41:28 +0300

    fix-image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 
 import './components/old-programm/style/global.css'
-// eslint-disable-next-line no-unused-vars
 import headerlogo from "./icons/header-logo.svg"
+import serachhader from './icons/search-header.svg'
+import menus1 from './icons/menus 1.svg'
+import trophy from './icons/trophy (1) 1.svg'
+import star from './icons/star 1.svg'
+import group from './icons/Group.svg'
+import group1 from './icons/Group (1).svg'
+import group2 from './icons/Group (2).svg'
+import layer from './icons/Layer_1.svg'
+import shopping from './icons/shopping-bag 2.svg'
+import checklist from './icons/checklist 2.svg'
+import stethoscope from './icons/stethoscope 1.svg'
+import location from './icons/location 1.svg'
+import picture from './icons/picture 1.svg'
+import laptop from './images/laptop.svg'
+import logoblack from './icons/logo-black.svg'
+import whatsapp from './icons/whatsapp (1) 2.svg'
+import VK from './icons/vk-social-network-logo 1.svg'
+import TG from './icons/telegram 1.svg'
+import logodown from './icons/logo-down.svg'
+
 function App() {
 
 
@@ -12,7 +31,7 @@ function App() {
                   <div className="header-info">
                       <img src={headerlogo} alt=""/>
                       <button className="header-info-button">
-                          <img className="button-icon" src="../src/icons/menus 1.svg" alt=""/>
+                          <img className="button-icon" src={menus1} alt=""/>
                           <p className="button-text">Все направления</p>
                       </button>
                       <p className="header-info-p">О сервисе</p>
@@ -22,7 +41,7 @@ function App() {
                       <p className="header-info-p">Контакты</p>
                   </div>
                   <div>
-                      <img src="../src/icons/search-header.svg" alt=""/>
+                      <img src={serachhader} alt=""/>
                   </div>
               </div>
               <div className="header-body">
@@ -53,7 +72,7 @@ function App() {
               <div className="section1-block1">
                   <div className="section1-info1">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/trophy (1) 1.svg" alt=""/>
+                          <img src={trophy} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text1">
@@ -70,7 +89,7 @@ function App() {
                   </div>
                   <div className="section1-info2">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/star 1.svg" alt=""/>
+                          <img src={star} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text3">
@@ -86,7 +105,7 @@ function App() {
                   </div>
                   <div className="section1-info3">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/Group.svg" alt=""/>
+                          <img src={group} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text5">
@@ -102,7 +121,7 @@ function App() {
                   </div>
                   <div className="section1-info4">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/Group (1).svg" alt=""/>
+                          <img src={group1} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text7">
@@ -111,14 +130,13 @@ function App() {
                           </p>
                           <p className="section1-p2 section1-text8">
                               Все рейтинги строятся на реальных данных, которые вносят сами компании.
-
                           </p>
                       </div>
 
                   </div>
                   <div className="section1-info2">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/Group (2).svg" alt=""/>
+                          <img src={group2} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text9">
@@ -134,7 +152,7 @@ function App() {
                   </div>
                   <div className="section1-info3">
                       <div className="section1-icon-block">
-                          <img src="../src/icons/Layer_1.svg" alt=""/>
+                          <img src={layer} alt=""/>
                       </div>
                       <div className='div-mr-l20'>
                           <p className="section1-p1 section1-text11">
@@ -159,7 +177,7 @@ function App() {
                       <div className="section1-content1">
                           <div className="section1-content1-head">
                               <div className="section1-content1-head-block">
-                                  <img className="section1-content1-head-icon" src="../src/icons/shopping-bag 2.svg" alt=""/>
+                                  <img className="section1-content1-head-icon" src={shopping} alt=""/>
                               </div>
                               <h2 className="section1-content1-head-h2">Торговля</h2>
 
@@ -178,7 +196,7 @@ function App() {
                       <div className="section1-content1">
                           <div className="section1-content1-head">
                               <div className="section1-content1-head-block">
-                                  <img className="section1-content1-head-icon" src="../src/icons/checklist 2.svg" alt=""/>
+                                  <img className="section1-content1-head-icon" src={checklist} alt=""/>
                               </div>
                               <h2 className="section1-content1-head-h2">Услуги</h2>
 
@@ -197,7 +215,7 @@ function App() {
                       <div className="section1-content1">
                           <div className="section1-content1-head">
                               <div className="section1-content1-head-block">
-                                  <img className="section1-content1-head-icon" src="../src/icons/stethoscope 1.svg" alt=""/>
+                                  <img className="section1-content1-head-icon" src={stethoscope} alt=""/>
                               </div>
                               <h2 className="section1-content1-head-h2">Здоровье</h2>
 
@@ -215,7 +233,7 @@ function App() {
                       <div className="section1-content1">
                           <div className="section1-content1-head">
                               <div className="section1-content1-head-block">
-                                  <img className="section1-content1-head-icon" src="../src/icons/location 1.svg" alt=""/>
+                                  <img className="section1-content1-head-icon" src={location} alt=""/>
                               </div>
                               <h2 className="section1-content1-head-h2">Путешествия и туризм</h2>
 
@@ -235,7 +253,7 @@ function App() {
                           <div className="section1-content1-head">
 
                               <div className="section1-content1-head-block">
-                                  <img className="section1-content1-head-icon" src="../src/icons/picture 1.svg" alt=""/>
+                                  <img className="section1-content1-head-icon" src={picture} alt=""/>
                               </div>
                               <h2 className="section1-content1-head-h2">Искусство</h2>
 
@@ -284,7 +302,7 @@ function App() {
                       </div>
 
                   </div>
-                  <img src="../src/images/laptop.svg" alt=""/>
+                  <img src={laptop} alt=""/>
               </div>
           </div>
 
@@ -384,7 +402,7 @@ function App() {
           </div>
           <div className="botton">
               <div className="botton-block1">
-                  <img src="../src/icons/logo-black.svg" alt=""/>
+                  <img src={logoblack} alt=""/>
                   <p>Сайт носит исключительно информационный характер и ни при каких условиях не является публичной
                       офертой, определяемой положениями ч. 2 ст. 437 Гражданского кодекса РФ. Чтобы получить подробную
                       информацию о стоимости услуг, обращайтесь, пожалуйста, к администраторам клиник. «Бест Клиник» -
@@ -490,15 +508,15 @@ function App() {
                           <h2>Соц сети и мессенджеры</h2>
                           <div className="botton-block-icon">
                               <div>
-                                  <img src="../src/icons/whatsapp (1) 2.svg" alt=""/>
+                                  <img src={whatsapp} alt=""/>
                               </div>
 
                               <div>
-                                  <img src="../src/icons/vk-social-network-logo 1.svg" alt=""/>
+                                  <img src={VK} alt=""/>
 
                               </div>
                               <div>
-                                  <img src="../src/icons/telegram 1.svg" alt=""/>
+                                  <img src={TG} alt=""/>
                               </div>
 
                           </div>
@@ -513,7 +531,7 @@ function App() {
               </div>
               <div className="down-block2">
                   <div><p>Сайт построили в</p></div>
-                  <div><img src="../src/icons/logo-down.svg" alt=""/></div>
+                  <div><img src={logodown} alt=""/></div>
 
               </div>
           </div>
